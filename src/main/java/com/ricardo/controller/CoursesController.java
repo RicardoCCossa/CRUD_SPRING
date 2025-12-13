45db3775804d1cc3f39b027908225f8df6489914
RicardoCCossa <ricardoccossa@gmail.com>
2025-12-13 21:23:53 +0200

    logica de remover

--- src/main/java/com/ricardo/controller/CoursesController.java
+++ src/main/java/com/ricardo/controller/CoursesController.java
@@ -3,9 +3,9 @@ package com.ricardo.controller;
 import com.ricardo.Repository.CourseRepository;
 import com.ricardo.model.Courses;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +19,40 @@ public class CoursesController {
     @GetMapping
     public List<Courses> lis() {
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Courses> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+       .map(recordFound -> ResponseEntity.ok().body(recordFound)).
+        orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Courses create(@RequestBody Courses course) {
+        return courseRepository.save(course);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Courses> update(@PathVariable Long id, @RequestBody Courses course) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    Courses updatedCourse = courseRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updatedCourse);
+                }).
+                orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    courseRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
+                }).
+                orElse(ResponseEntity.notFound().build());
     }
 }
